dbfcfd6ff9e52070679b0575a79405a7336767e8
Yauheni13 <yauheni.butsko@gmail.com>
2018-07-07 17:45:15 -0400

    initial commit

---
+++ JenkinsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JenkinsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JenkinsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac6febd4-d08f-43da-b829-61b8ce8ce548")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JenkinsTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace JenkinsTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void GoogleTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(@"https://google.com");
+            Thread.Sleep(5000);
+        }
+    }
+}
